32f72f2d222ce391fd8425e1dab107e60fdf7b5a
Philip Newcomer <contact@philipnewcomer.net>
2016-07-27 08:30:34 -0400

    Remove no longer needed wp-config directives for a subdirectory install

--- public/wp-config.php
+++ public/wp-config.php
@@ -2,11 +2,10 @@
 /**
  * The base WordPress configuration file
  *
- * The purpose of this file is fourfold:
+ * The purpose of this file is threefold:
  *  1) Load the local configuration file
- *  2) Define constants that tell WordPress it is installed in the /wordpress subdirectory
- *  3) Define the custom wp-content directory
- *  4) Bootstrap WordPress itself
+ *  2) Define the custom wp-content directory
+ *  3) Bootstrap WordPress itself
  *
  * Do not store any sensitive information in this file, as it is part of the shared repository. Place sensitive
  * information, such as database credentials, salts, and anything specific to an installation in wp-local-config.php.
@@ -17,13 +16,8 @@ if ( file_exists( dirname( __DIR__ ) . '/wp-local-config.php' ) ) {
 	require_once( dirname( __DIR__ ) . '/wp-local-config.php' );
 }
 
-// Define the home and site URLs.
-define( 'WP_HOME',    ( empty( $_SERVER['https'] ) ? 'http://' : 'https://' ) . ( isset( $_SERVER['HTTP_HOST'] ) ? $_SERVER['HTTP_HOST'] : '' ) );
-define( 'WP_SITEURL', WP_HOME . '/wordpress' );
-
 // Define the wp-content directory.
 define( 'WP_CONTENT_DIR', __DIR__ . '/wp-content' );
-define( 'WP_CONTENT_URL', WP_HOME . '/wp-content' );
 
 // Define ABSPATH and load WordPress.
 if ( ! defined( 'ABSPATH' ) ) {
